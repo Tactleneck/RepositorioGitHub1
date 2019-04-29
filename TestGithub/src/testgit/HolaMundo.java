19322916ea46b66a4fd9fb8b39b46b2895259a69
Archer <Archer@DESKTOP-0AE99J8>
2019-04-29 04:38:47 +0200

    Crear master1

---
+++ TestGithub/src/testgit/HolaMundo.java
@@ -0,0 +1,20 @@
+package testgit;
+
+public class HolaMundo {
+	
+	HolaMundo(){
+		
+	}
+	
+	void hola () {
+		System.out.println("Hola Primera Version 11");
+	}
+
+	public static void main(String[] args) {
+		HolaMundo obj1 = new HolaMundo();
+		obj1.hola();
+		
+
+	}
+
+}
